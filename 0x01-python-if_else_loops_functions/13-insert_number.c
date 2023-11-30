d4beb647aa1ee2a0deaff2e5af737053479e12f0
Shalifloxy <agbaicynthia46@gmail.com>
2023-11-29 18:10:14 -0800

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,53 +1,36 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-#include "lists.h"
+#include <stdlib.h>
 
-/**
- * insert_node - inserts a node in a proper place of sorted list
- * @head: first node of linked list
- * @number: number to add in linked list
- *
- * Return: Address of new node or NULL if failure
- */
-listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *n_node;
-	listint_t *tmp;
+/* Definition of a singly linked list node */
+typedef struct listint_s {
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-	if (!head)
-		return (NULL);
+/* Function to insert a number into a sorted singly linked list */
+listint_t *insert_node(listint_t **head, int number) {
+    listint_t *new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL) {
+        return NULL; // Return NULL if memory allocation fails
+    }
+    new_node->n = number;
+    new_node->next = NULL;
 
-	n_node = malloc(sizeof(listint_t));
-	if (!n_node)
-		return (NULL);
-	n_node->n = number;
+    listint_t *current = *head;
+    listint_t *prev = NULL;
 
-	tmp = *head;
-	if (tmp)
-	{
-		if (tmp->n >= number)
-		{
-			n_node->next = tmp;
-			*head = n_node;
-		}
-		else
-		{
-			while (tmp->next)
-			{
-				if (tmp->next->n < number)
-					tmp = tmp->next;
-				else
-					break;
-			}
-			n_node->next = tmp->next;
-			tmp->next = n_node;
-		}
-	}
-	else
-	{
-		*head = n_node;
-		n_node->next = NULL;
-	}
-	return (n_node);
+    while (current != NULL && current->n < number) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (prev == NULL) {
+        new_node->next = *head;
+        *head = new_node;
+    } else {
+        prev->next = new_node;
+        new_node->next = current;
+    }
+
+    return new_node; // Return the address of the new node
 }
