49331632bfacd4a1f3272850cbfe7eb5ee042b9f
Shalifloxy <agbaicynthia46@gmail.com>
2023-11-29 18:27:24 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 for char in range(ord('z'), ord('A') - 1, -1):
-    if char % 2 == 1:
-        print(chr(char), end='')
-    else:
-        print(chr(char), end='')
+    print('{:c}'.format(char if char % 2 == 1 else char + 32), end='')
