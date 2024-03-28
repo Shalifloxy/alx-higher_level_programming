c2d03de99a74ac8ab322db6604498acfdf8054de
“Shalifloxy” <agbaicynthia46@gmail.com>
2024-03-28 05:17:51 -0700

    all

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,36 +1,55 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
-/* Definition of a singly linked list node */
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
+/**
+ * insert_node - Inserts a number into a sorted singly linked list.
+ *
+ * @head: Double pointer to a singly linked list
+ *
+ * @number: Value of the new node.
+ *
+ * Return: The address of the new node, or NULL if it failed.
+ */
 
-/* Function to insert a number into a sorted singly linked list */
-listint_t *insert_node(listint_t **head, int number) {
-    listint_t *new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL) {
-        return NULL; // Return NULL if memory allocation fails
-    }
-    new_node->n = number;
-    new_node->next = NULL;
+listint_t *insert_node(listint_t **head, int number)
+{
+	int flag = 0;
+	listint_t *new_node = NULL, *actual = NULL, *after = NULL;
 
-    listint_t *current = *head;
-    listint_t *prev = NULL;
-
-    while (current != NULL && current->n < number) {
-        prev = current;
-        current = current->next;
-    }
-
-    if (prev == NULL) {
-        new_node->next = *head;
-        *head = new_node;
-    } else {
-        prev->next = new_node;
-        new_node->next = current;
-    }
-
-    return new_node; // Return the address of the new node
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = number, new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	actual = *head;
+	if (number <= actual->n)
+	{
+		new_node->next = actual, *head = new_node;
+		return (*head);
+	}
+	if (number > actual->n && !actual->next)
+	{
+		actual->next = new_node;
+		return (new_node);
+	}
+	after = actual->next;
+	while (actual)
+	{
+		if (!after)
+			actual->next = new_node, flag = 1;
+		else if (after->n == number)
+			actual->next = new_node, new_node->next = after, flag = 1;
+		else if (after->n > number && actual->n < number)
+			actual->next = new_node, new_node->next = after, flag = 1;
+		if (flag)
+			break;
+		after = after->next, actual = actual->next;
+	}
+	return (new_node);
 }
